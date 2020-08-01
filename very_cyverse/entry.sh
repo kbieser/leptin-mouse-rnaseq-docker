b445a9cbf89ce08acb0d6f731928c6675d60647c
kbieser <kayla.bieser@nsc.edu>
2020-08-01 14:33:10 -0700

    added source line

--- very_cyverse/entry.sh
+++ very_cyverse/entry.sh
@@ -2,6 +2,8 @@
 
 HOME=/home/gea_user
 
+source ~/.bashrc
+
 #echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
 #exec jupyter lab --no-browser --allow-root --ip=0.0.0.0
