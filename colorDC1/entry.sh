d437877abcaa9f95962524bb8edd8403426f9c44
kbieser <kayla.bieser@nsc.edu>
2020-08-03 10:00:36 -0700

    run new user

--- colorDC1/entry.sh
+++ colorDC1/entry.sh
@@ -2,7 +2,7 @@
 
 HOME=/home/gea_user
 
-source ~/.bashrc
+#source ~/.bashrc
 
 #echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
