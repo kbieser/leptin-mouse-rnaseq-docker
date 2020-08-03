c96e171b5519c439ff6bac0e21fa1a54d6994127
kbieser <kayla.bieser@nsc.edu>
2020-08-03 12:44:33 -0700

    colorized and edit header

--- colorDC1/entry.sh
+++ colorDC1/entry.sh
@@ -2,9 +2,6 @@
 
 HOME=/home/gea_user
 
-#source ~/.bashrc
-
-
 #echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
 #exec jupyter lab --no-browser --allow-root --ip=0.0.0.0
